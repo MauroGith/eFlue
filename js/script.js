a6332eab3dd5d265dcab72fd706d07cc925dd896
MarcosVinicius98 <caconk2013@hotmail.com>
2023-02-21 20:19:49 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const ctx = document.getElementById('myChart');
+    Chart.defaults.font.size = '7px'
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['00:00-02:00', '02:00-04:00', '04:00-06:00', '06:00-08:00', '08:00-10:00', '10:00-12:00', '12:00-14:00', '14:00-16:00', '16:00-18:00', '18:00-20:00'],
+      datasets: [{
+        label: '',
+        data: [100, 200, 300, 40, 100, 250, 710, 880, 990, 1000],
+        borderWidth: 3,
+        borderColor: "#f50066",
+        tension: 0.3,
+        pointRadius: 0,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            display: false
+          }
+        }
+      },
+      plugins: {
+        legend: {
+            display: false
+        }
+      }, 
+    }
+  });
+  
